ca88672bb173e70e9ab94d0c6f19f150746d8e66
John Doe <zalmanstefan2004@gmail.com>
2021-10-18 16:33:01 +0300

    started hwmap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/RobotHardware.java
@@ -12,15 +12,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 public class RobotHardware {
+	public HardwareMap hardwareMap;
 
-	private final ElapsedTime period = new ElapsedTime();
-	public BNO055IMU imu;
-	public DcMotorEx frontLeftMotor = null;
-	public DcMotorEx frontRightMotor = null;
-	public DcMotorEx backLeftMotor = null;
-	public DcMotorEx backRightMotor = null;
-	HardwareMap hardwareMap = null;
-
+	public DcMotorEx leftMotor = null;
+	public DcMotorEx rightMotor = null;
+	public BNO055IMU imu = null;
 	/**
 	 * Sets up the HardwareMap
 	 *
@@ -28,7 +24,11 @@ public class RobotHardware {
 	 */
 	public void init(HardwareMap hardwareMap) {
 		this.hardwareMap = hardwareMap;
-		// imu set up parameters
+
+		leftMotor = hardwareMap.get(DcMotorEx.class, "left_Motor");
+		rightMotor = hardwareMap.get(DcMotorEx.class, "right_Motor");
+
+
 		BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 		parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
 		parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -42,38 +42,5 @@ public class RobotHardware {
 		imu.initialize(parameters);
 		imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-
-		frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
-		frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
-		backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
-		backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
-
-		// Motor Directions
-		frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
-		frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-		backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-		backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-		frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-		frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
-		frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-		frontRightMotor.setPower(0);
-		frontLeftMotor.setPower(0);
-		backRightMotor.setPower(0);
-		backLeftMotor.setPower(0);
 	}
 }
