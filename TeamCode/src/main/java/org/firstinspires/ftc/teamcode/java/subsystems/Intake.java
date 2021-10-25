d01a3390c5ee995ead0ebb8f7a1c47b755a9cff1
Sz77 <zalmanstefan2004@gmail.com>
2021-10-25 22:21:25 +0300

    fix Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/subsystems/Intake.java
@@ -7,32 +7,37 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
 
-public class Intake extends RobotHardware
+public class Intake
 {
-
+    RobotHardware robot;
     private DcMotorEx intake;
 
     public static double intake_speed = 1;
     public static double outtake_speed = -1;
 
-
-    public void intake(double speed)
-    {
-        intake.setPower(intake_speed);
+    /**
+     * this function creates anew intake
+     * @param robot the robot hardware
+     */
+    public Intake(RobotHardware robot) {
+        this.robot = robot;
+        this.intake = robot.intake;
     }
 
-    public void outtake()
-    {
-        intake.setPower(outtake_speed);
-    }
+    /**
+     * this function intakes
+     */
+    public void intake() { intake.setPower(intake_speed); }
 
-    public void stop()
-    {
-        intake(0.0);
-    }
+    /**
+     * this function outtakes
+     */
+    public void outtake() { intake.setPower(outtake_speed); }
 
-    public void debug()
-    {
+    /**
+     * this function turns off the intake
+     */
+    public void stop() { intake.setPower(0); }
 
-    }
+    public void debug() {}
 }
