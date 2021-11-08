514085174cddb717dfd9351308e22cb635dc3a15
Emily <emily080806@gmail.com>
2021-11-08 14:24:24 +0200

    Create Carousel.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/subsystems/Carousel.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.java.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
+
+public class Carousel {
+	RobotHardware robot;
+	private final DcMotor carousel;
+	private static final double CAROUSEL_SPEED = 1;
+
+	private boolean isReversed = false;
+
+	/**
+	 * this function is the constructor for the carousel subsystem
+	 * @param robot the robot hardware
+	 */
+	public Carousel(RobotHardware robot) {
+		this.robot = robot;
+		this.carousel = robot.carousel;
+	}
+
+	/**
+	 * this function stops the carousel
+	 */
+	public void stop() {
+		carousel.setPower(0);
+	}
+
+	/**
+	 * this function starts the carousel
+	 */
+	public void spin (){
+		carousel.setPower(CAROUSEL_SPEED);
+	}
+
+	/**
+	 * this function changes the direction of the carousel
+	 */
+	public void changeDirection() {
+		if (isReversed){
+			carousel.setDirection(DcMotor.Direction.FORWARD);
+			isReversed = false;
+		}else {
+			carousel.setDirection(DcMotor.Direction.REVERSE);
+			isReversed = true;
+		}
+	}
+}
