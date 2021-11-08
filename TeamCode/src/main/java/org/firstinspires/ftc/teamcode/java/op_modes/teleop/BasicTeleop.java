6e44387115d8cce41e3a3f7b5df50af7cd7cfb23
Sz77 <zalmanstefan2004@gmail.com>
2021-11-08 19:21:01 +0200

    Fix formatting and misspells

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/BasicTeleop.java
@@ -3,8 +3,7 @@ package org.firstinspires.ftc.teamcode.java.op_modes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
+
 
 import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
 
@@ -17,7 +16,7 @@ public class BasicTeleop extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
 
-    private  double maxSpeed = 1;
+    private final double MAX_SPEED = 1;
 
 
     @Override
@@ -26,42 +25,35 @@ public class BasicTeleop extends LinearOpMode {
 
 
         robot.init(hardwareMap);
-        /**
-         * defining motors
-         */
+
+         // defining motors
         DcMotor leftMotor = robot.leftMotor;
         DcMotor rightMotor = robot.rightMotor;
-        /**
-         * creating an arry for the motor speeds
-         */
 
-        double motorSpeeds [] = new double[2];
+         // creating an array for the motor speeds
+        double[] motorSpeeds = new double[2];
 
         waitForStart();
 
         while (opModeIsActive())
         {
-            /**
-             * corolating gamepad sticks to driving states
-             */
+
+             // correlating gamepad sticks to driving states
             double drive = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
 
-            /**
-             * creating aray for motor speeds that takes a value from the diffrent driving states
-             */
+
+             // creating array for motor speeds that takes a value from the different driving states
             motorSpeeds[0] = drive + turn;
             motorSpeeds[1] = drive - turn;
 
-            /**
-             * defining max speed for motors
-             */
+
+             // defining max speed for motors
             double max = Math.max(Math.abs(motorSpeeds[0]),Math.abs(motorSpeeds[1]));
 
 
-            /**
-             * creating a stable range for the motor max speed
-             */
+
+             // creating a stable range for the motor max speed
             if (max > 1)
             {
                 for(int i = 0 ; i < 2; i++)
@@ -70,12 +62,11 @@ public class BasicTeleop extends LinearOpMode {
                 }
             }
 
-            /**
-             * setting power to the motors based the values of the speeds from the
-             * gamepad and max speed
-             */
-            leftMotor.setPower(motorSpeeds[0] * maxSpeed);
-            rightMotor.setPower(motorSpeeds[1] * maxSpeed);
+
+             // setting power to the motors based the values of the speeds from the
+             // gamepad and max speed
+            leftMotor.setPower(motorSpeeds[0] * MAX_SPEED);
+            rightMotor.setPower(motorSpeeds[1] * MAX_SPEED);
         }
     }
 }
