de09aba43e3320f807fa84ca7271c883e96f71a3
kingtop1gg <94991114+kingtop1gg@users.noreply.github.com>
2021-12-03 13:09:04 +0200

    comment out old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/PositionControl/PidfController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/util/PositionControl/PidfController.java
@@ -1,92 +1,92 @@
-package org.firstinspires.ftc.teamcode.java.util.PositionControl;
-
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-/**
- * the PIDController class will be used for  all the different pid calculations
- * for example AutoDriving ,autoAdjusting , controlling the shooter speed
- */
-//P is main power, I looks at the sum of error and gives final push, D is how much the error is changing
-public class PidfController {
-	//TODO look at using System instead of ElapsedTime
-	private final ElapsedTime elapsedTime;
-	double integral = 0;
-	double derivative = 0;
-	private double maxI = .1;
-	private double minI = -.1;
-	private double previousTime;
-	private double kp;
-	private double ki;
-	private double kd;
-	private double f;
-
-	public PidfController(double kp, double ki, double kd, double f) {
-		elapsedTime = new ElapsedTime();
-		this.previousTime = 0;
-		this.kp = kp;
-		this.ki = ki;
-		this.kd = kd;
-		this.f = f;
-	}
-
-	public double getMaxIntegral() {
-		return maxI;
-	}
-
-	public void setMaxIntegral(double maxI) {
-		this.maxI = maxI;
-	}
-
-	public double getMinIntegral() {
-		return minI;
-	}
-
-	public void setMinIntegral(double minI) {
-		this.minI = minI;
-	}
-
-	public double getP() {
-		return kp;
-	}
-
-	public void setP(double kp) {
-		this.kp = kp;
-	}
-
-	public double getI() {
-		return ki;
-	}
-
-	public void setI(double ki) {
-		this.ki = ki;
-	}
-
-	public double getD() {
-		return kd;
-	}
-
-	public void setD(double kd) {
-		this.kd = kd;
-	}
-
-	public double getF() {
-		return f;
-	}
-
-	public void setF(double f) {
-		this.f = f;
-	}
-
-	public double calculate(double error) {
-		double currentTime = elapsedTime.nanoseconds();
-		double p = kp * error;
-		double i = Range.clip(integral + ki * (error * (currentTime - previousTime)), minI, maxI);
-		integral = i;
-		double d = kd * ((error - derivative) / (currentTime - previousTime));
-		derivative = error;
-		previousTime = currentTime;
-
-		return f + (1 - f) * (p + i + d);
-	}
-}
+//package org.firstinspires.ftc.teamcode.java.util.PositionControl;
+//
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.Range;
+//
+///**
+// * the PIDController class will be used for  all the different pid calculations
+// * for example AutoDriving ,autoAdjusting , controlling the shooter speed
+// */
+////P is main power, I looks at the sum of error and gives final push, D is how much the error is changing
+//public class PidfController {
+//	//TODO look at using System instead of ElapsedTime
+//	private final ElapsedTime elapsedTime;
+//	double integral = 0;
+//	double derivative = 0;
+//	private double maxI = .1;
+//	private double minI = -.1;
+//	private double previousTime;
+//	private double kp;
+//	private double ki;
+//	private double kd;
+//	private double f;
+//
+//	public PidfController(double kp, double ki, double kd, double f) {
+//		elapsedTime = new ElapsedTime();
+//		this.previousTime = 0;
+//		this.kp = kp;
+//		this.ki = ki;
+//		this.kd = kd;
+//		this.f = f;
+//	}
+//
+//	public double getMaxIntegral() {
+//		return maxI;
+//	}
+//
+//	public void setMaxIntegral(double maxI) {
+//		this.maxI = maxI;
+//	}
+//
+//	public double getMinIntegral() {
+//		return minI;
+//	}
+//
+//	public void setMinIntegral(double minI) {
+//		this.minI = minI;
+//	}
+//
+//	public double getP() {
+//		return kp;
+//	}
+//
+//	public void setP(double kp) {
+//		this.kp = kp;
+//	}
+//
+//	public double getI() {
+//		return ki;
+//	}
+//
+//	public void setI(double ki) {
+//		this.ki = ki;
+//	}
+//
+//	public double getD() {
+//		return kd;
+//	}
+//
+//	public void setD(double kd) {
+//		this.kd = kd;
+//	}
+//
+//	public double getF() {
+//		return f;
+//	}
+//
+//	public void setF(double f) {
+//		this.f = f;
+//	}
+//
+//	public double calculate(double error) {
+//		double currentTime = elapsedTime.nanoseconds();
+//		double p = kp * error;
+//		double i = Range.clip(integral + ki * (error * (currentTime - previousTime)), minI, maxI);
+//		integral = i;
+//		double d = kd * ((error - derivative) / (currentTime - previousTime));
+//		derivative = error;
+//		previousTime = currentTime;
+//
+//		return f + (1 - f) * (p + i + d);
+//	}
+//}
