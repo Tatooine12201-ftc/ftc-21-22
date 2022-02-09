32f42c5eea8f2f3f3c0438c0f61dac5b3c2c3318
Emily Kapustin <87940471+emily09876@users.noreply.github.com>
2022-02-09 17:36:40 +0200

    12566

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/blueduks.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.java.op_modes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.java.subsystems.Carousel;
+import org.firstinspires.ftc.teamcode.java.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.java.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.java.util.AutoDrive;
+import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
+
+@Autonomous(name = "blueDucks", group = "auto")
+public class blueduks  extends LinearOpMode {
+    /* Declare OpMode members. */
+    RobotHardware robot   = new RobotHardware();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+    /**
+     * Override this method and place your code here.
+     * <p>
+     * Please do not swallow the InterruptedException, as it is used in cases
+     * where the op mode needs to be terminated early.
+     *
+     *
+     */
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        robot.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        DcMotor carouselMotor = robot.carousel;
+        AutoDrive ad = new AutoDrive(robot.leftMotor, robot.rightMotor, robot.imu, telemetry);
+        Carousel carousel = new Carousel(carouselMotor);
+        //Lift lift = new Lift(robot.elevator);
+        ad.gyroDrive(AutoDrive.DRIVE_SPEED,580,0);
+        ad.gyroTurn(AutoDrive.TURN_SPEED,35);
+        ad.gyroDrive(AutoDrive.DRIVE_SPEED,-520,0);
+        while (runtime.seconds() > 5);{
+            carousel.spin();
+        }
+
+
+    }
+}
