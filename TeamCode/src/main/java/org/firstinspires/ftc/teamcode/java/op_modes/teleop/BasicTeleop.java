8016ef3bf415ffc94d6d2ced488599ccf3e4e8f8
agam200615 <88667009+agam200615@users.noreply.github.com>
2022-03-07 16:23:37 +0200

    12567

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/op_modes/teleop/BasicTeleop.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.java.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.java.subsystems.Lift;
 import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
 
-//import org.firstinspires.ftc.teamcode.java.subsystems.Capping;
+import org.firstinspires.ftc.teamcode.java.subsystems.Capping;
 
 
 @TeleOp(name = "BasicTeleop")
